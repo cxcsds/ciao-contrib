cb9048a0d5b6c829d0dba029231929d74d760bac
Douglas Burke <dburke.gw@gmail.com>
2021-10-25 09:48:32 -0400

    Initial version of PEP 517/518 work
    Move most of the setup from setup.py to setup.cfg but not
    everything. It should be possible to do this automatically,
    probably with setuptools_scm, but leave that for now.
    
    The default version is now 4.14.0 rather than 4.13.0.

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 """
 Create the CIAO contributed package. Options include:
 
-  --version=4.13.0
+  --version=4.14.0
 
 """
 
 import glob
 import sys
 
-from distutils.core import setup
+from setuptools import setup
+
 
 def list_files(pattern):
     "Return the giles in a directory, in a sorted list"
@@ -17,7 +18,7 @@ def list_files(pattern):
     # Remove *~ files (any other ones?) Could restrict to
     # files stored in git?
     #
-    files = [f for f in files if not f.endswith('~')]
+    files = [f for f in files if not (f.endswith('~') or f.startswith('flycheck_'))]
 
     if files == []:
         raise ValueError(f"No match for pattern: {pattern}")
@@ -25,7 +26,7 @@ def list_files(pattern):
     return sorted(files)
 
 
-VERSION="4.13.0"
+VERSION = "4.14.0"
 for val in sys.argv:
     if val.startswith("--version="):
         VERSION = val.split("=")[1]
@@ -107,30 +108,9 @@ for dname in ["install", "install/readline",
     data_files.append((outdir, list_files(outdir + "/*.h")))
 
 
-mods = [ "ciao_contrib",
-    "ciao_contrib/region",
-    "ciao_contrib/_tools",
-    "ciao_contrib/cda",
-    "coords",
-    "crates_contrib",
-    "dax",
-    "sherpa_contrib",
-    "sherpa_contrib/profiles",
-    "sherpa_contrib/tests",
-    "sherpa_contrib/xspec"]
-
-setup(name='ciao-contrib',
-      version=VERSION,
-      license='GNU GPL v3',
-      description='CIAO Contributed scripts',
-      author='CXCSDS and Friends',
-      author_email='cxchelp@cfa.harvard.edu',
-      url='https://github.com/cxcsds/ciao-contrib/',
+setup(version=VERSION,
       scripts=scripts,
 
       # Note that data_files is deprecated
       data_files=data_files,
-
-      packages=mods,
-      py_modules=["lightcurves",]
 )
