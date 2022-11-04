ce295b3c6b609f5cbcda2df6c9aec0ab7a16881f
Doug Burke <dburke@cfa.harvard.edu>
2022-11-04 10:07:53 -0400

    Remove support for chips from sherpa_contrib.chart/marx

--- sherpa_contrib/chart.py
+++ sherpa_contrib/chart.py
@@ -303,8 +303,6 @@ def plot_chart_spectrum(id=None, elow=None, ehigh=None, ewidth=None,
     # LaTeX support depends on the backend
     if plot.backend.name == 'pylab':
         hplot.ylabel = 'Flux (photon cm$^{-2}$ s$^{-1}$)'
-    elif plot.backend.name == 'chips':
-        hplot.ylabel = 'Flux (photon cm^{-2} s^{-1})'
     else:
         hplot.ylabel = 'Flux (photon cm^-2 s^-1)'
 
