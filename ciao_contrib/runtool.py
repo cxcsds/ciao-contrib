6a68305fa4ff511a5f5e723ebff8aad67ffd5036
Ken Glotfelty <kglotfelty@cfa.harvard.edu>
2023-02-03 13:06:33 -0500

    get_dither_parameter: estimate dither parameters from asol file

--- ciao_contrib/runtool.py
+++ ciao_contrib/runtool.py
@@ -3199,6 +3199,13 @@ parinfo['geom'] = {
     }
 
 
+parinfo['get_dither_parameters'] = {
+    'istool': True,
+    'req': [ParValue("infile","f","Input aspect solution file",None)],
+    'opt': [ParSet("method","s","Method to estimate dither parameters",'fold',["fold","fit","fft"]),ParValue("dety_amplitude","r","Amplitude in DETY direction [deg]",None),ParValue("detz_amplitude","r","Amplitude in the DETZ direction [deg]",None),ParValue("dety_period","r","Period in DETY direction [cycle/sec]",None),ParValue("detz_period","r","Period in DETZ direction [cycle/sec]",None),ParValue("tmpdir","s","Directory for temporary files",'${ASCDS_WORK_PATH}'),ParRange("verbose","i","Amount of tool chatter",1,0,5)],
+    }
+
+
 parinfo['get_fov_limits'] = {
     'istool': True,
     'req': [ParValue("infile","f","The FOV file to use",None)],
